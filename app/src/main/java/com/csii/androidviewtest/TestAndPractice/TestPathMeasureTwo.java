e51a85d00c61d010a02aeed34cc4c94c72579de9
zqhead <zqhead@sina.com>
2018-02-05 18:04:19 +0800

    add some code details

--- app/src/main/java/com/csii/androidviewtest/TestAndPractice/TestPathMeasureTwo.java
+++ app/src/main/java/com/csii/androidviewtest/TestAndPractice/TestPathMeasureTwo.java
@@ -6,10 +6,14 @@ import android.util.AttributeSet;
 import android.view.View;
 
 /**
+ * 测试pathMeasure的复杂方法 getPosTan 和 getMatrix
  * Created by zq on 2018/2/4.
  */
 
 public class TestPathMeasureTwo extends View {
+    private float[] pos;
+    private float[] tan;
+
     public TestPathMeasureTwo(Context context) {
         super(context);
         init();
