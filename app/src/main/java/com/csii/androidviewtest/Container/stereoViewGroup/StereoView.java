480662cd73f6ef30e874c0ecd3bce3a817be3027
zqhead <zqhead@sina.com>
2018-03-28 18:01:03 +0800

    add some code details in stereoView, and prepare to test Fling and velocityTracker in TestScrollerOne

--- app/src/main/java/com/csii/androidviewtest/Container/stereoViewGroup/StereoView.java
+++ app/src/main/java/com/csii/androidviewtest/Container/stereoViewGroup/StereoView.java
@@ -2,6 +2,7 @@ package com.csii.androidviewtest.Container.stereoViewGroup;
 
 import android.content.Context;
 import android.graphics.Camera;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -18,15 +19,23 @@ import com.csii.androidviewtest.Util.LogUtil;
  */
 
 public class StereoView extends ViewGroup {
-    private int index;
+    private int StartIndex;//其实状态显示第几个子控件
 
     private Camera mCamera;
     private Matrix mMatrix;
     private Scroller mScroller;
-
+    /**
+     * 触发滑动的最小距离
+     * */
     private int minTouchSlop;
 
+    /**
+     * ACTION_DOWN时的X坐标
+     * */
     private float mXDown;
+    /**
+     * ACTION_DOWN时的T坐标
+     * */
     private float mYDown;
 
     private int mWidth;
@@ -34,6 +43,16 @@ public class StereoView extends ViewGroup {
     private int mCurrentItem;
     private VelocityTracker mVelocityTracker;
 
+    private int standardSpeed;
+
+    /**
+     * 两个控件之间的夹角
+     * */
+    private int mAngle;
+
+    /**
+     * 表示touch动作是否被拦截的标识
+     * */
     private boolean isCanFling;
 
     public StereoView(Context context) {
@@ -52,8 +71,9 @@ public class StereoView extends ViewGroup {
         mCamera = new Camera();
         mScroller = new Scroller(context);
 
-        index = 1;
+        StartIndex = 1;
         mCurrentItem = 0;
+        mAngle = 90;
     }
 
     @Override
@@ -63,7 +83,8 @@ public class StereoView extends ViewGroup {
             measureChild(child, widthMeasureSpec, heightMeasureSpec);
         }
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        //scrollTo(0, index * getMeasuredHeight());
+        //移动到初始显示的控件
+        scrollTo(0, StartIndex * getMeasuredHeight());
     }
 
     @Override
@@ -109,13 +130,17 @@ public class StereoView extends ViewGroup {
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 LogUtil.i("111111");
+                //记录down动作的坐标
                 mXDown = ev.getX();
                 mYDown = ev.getY();
                 break;
             case MotionEvent.ACTION_MOVE:
                 LogUtil.i("22222222");
+                //记录move动作的坐标
                 float mXMove = ev.getX();
                 float mYMove = ev.getY();
+
+                //计算viewGroup是否拦截事件
                 isCanFling = isCanScrolling(mXMove, mYMove);
                 if (isCanFling == true){
                     return isCanFling;
@@ -128,6 +153,7 @@ public class StereoView extends ViewGroup {
     }
 
     private boolean isCanScrolling(float mXMove, float mYMove) {
+        //如果y方向的移动大于最小触发移动事件距离，并且Y轴距离大于x轴距离时，返回true
         if ((Math.abs(mYMove - mYDown) > minTouchSlop) && (Math.abs(mYMove - mYDown) > Math.abs(mXMove - mXDown))) {
             return true;
         } else return false;
@@ -141,23 +167,30 @@ public class StereoView extends ViewGroup {
         mVelocityTracker.addMovement(event);
         switch (event.getAction()) {
             case MotionEvent.ACTION_MOVE:
-
+                //计算Y移动方向，然后根据方向，进行滚动
                 float y = event.getY();
                 float realDelta = mYDown - y;//scrollTo 和scrollBy y轴方向都是正数向上移动 复数向下移动，和屏幕坐标系正好相反，所以需要用起始点去减终点，这样才能适配
                 mYDown = y;
+                //进行滚动
                 recycleYMove(realDelta);
                 break;
             case MotionEvent.ACTION_CANCEL:
             case MotionEvent.ACTION_UP:
                 isCanFling = false;
+                mVelocityTracker.computeCurrentVelocity(1000);
 //                int currentIndex = (getScrollY() + mHeight / 2)/ mHeight;
 //                int dy = currentIndex * mHeight - getScrollY();
-
-                int Y = getScrollY() % mHeight;
-                int dy = Y <= mHeight / 2 ? - Y : mHeight - Y;
-                LogUtil.i("ACTION_UP",getScrollY() + "," + Y + ","+ dy);
-                mScroller.startScroll(0, getScrollY(), 0, dy);
-                invalidate();
+                float yCurrentSpeed = mVelocityTracker.getYVelocity();
+//                if( yCurrentSpeed > standardSpeed && getScrollY() % mHeight > mHeight / 2) {
+//
+//                } else {
+                    //抬手时，根据现实的两个子VIew的位置就行 滑动复位
+                    int Y = getScrollY() % mHeight;
+                    int dy = Y <= mHeight / 2 ? - Y : mHeight - Y;
+                    LogUtil.i("ACTION_UP",getScrollY() + "," + Y + ","+ dy);
+                    mScroller.startScroll(0, getScrollY(), 0, dy);
+                    invalidate();
+//                }
                 break;
 
         }
@@ -168,11 +201,11 @@ public class StereoView extends ViewGroup {
     private void recycleYMove(float realDelta) {
         int yDelta = (int)realDelta % mHeight;
         scrollBy(0, (int)yDelta);
-        if (getScrollY() < 5) {
+        if (getScrollY() < 0) {
             LogUtil.i("22222222");
             addPre();
             scrollBy(0, mHeight);
-        } else if( getScrollY() > mHeight *(getChildCount()- 1) - 5) {
+        } else if( getScrollY() > mHeight *(getChildCount()- 1)) {
             LogUtil.i("3333333333");
             addNext();
             scrollBy(0, -mHeight);
@@ -196,6 +229,52 @@ public class StereoView extends ViewGroup {
         mCurrentItem = (mCurrentItem + 1) % getChildCount();
     }
 
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        for (int i = 0; i < getChildCount(); i++) {
+            drawChildScreen(canvas, i , getDrawingTime());
+        }
+        //super.dispatchDraw(canvas);
+    }
+
+
+    private void drawChildScreen(Canvas canvas, int i , long drawingTime) {
+        int childCurrentY = i * mHeight;
+        //当子View的控件上边沿不在这两个范围之内，则寿命ziVIew不在可视范围之内，则不用绘制
+        if (getScrollY() - mHeight >  childCurrentY) { //上一个child的上边沿边界最低值
+            return;
+        } else if (childCurrentY > getScrollY() + mHeight) { //下一个child的上边沿的最大值
+            return;
+        }
+        //根据高度差值计算子VIew的旋转角度
+        // 上一个子view的上边沿高于可视范围上沿（childCurrentY < getScrollY()），则角度为正 旋转中心为上一个子下边沿中点
+        // 下一个子view的上沿该与可视范围的下沿（childCurrentY > getScrollY()），则角度为负 旋转中心为下一个子上边沿中点
+
+        //计算两个子View的旋转角度 两个子View的旋转中心其实是相同的
+        int currAngle = mAngle * (getScrollY() - childCurrentY) / mHeight;
+        if (currAngle > 90 || currAngle < -90) {
+            return;
+        }
+        //计算旋转中心
+        int centreX = mWidth / 2;
+        int centreY = childCurrentY < getScrollY() ? childCurrentY + mHeight : childCurrentY;
+
+        //进行旋转
+        mCamera.save();
+        mCamera.rotateX(currAngle);
+        mCamera.getMatrix(mMatrix);
+        mCamera.restore();
+
+        //移动旋转中心
+        mMatrix.preTranslate( -centreX, -centreY);
+        mMatrix.postTranslate( centreX,  centreY);
+
+        //相乘计算好后的矩阵 然后绘制
+        canvas.save();
+        canvas.concat(mMatrix);
+        drawChild(canvas, getChildAt(i), drawingTime);
+        canvas.restore();
+    }
 
     @Override
     public void computeScroll() {
@@ -203,5 +282,17 @@ public class StereoView extends ViewGroup {
             scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
             postInvalidate();
         }
+    }
+
+    public void toPre() {
+
+    }
+
+    public void toNext() {
+
+    }
+
+    public void toSpecificItems (int items) {
+
     }
 }
